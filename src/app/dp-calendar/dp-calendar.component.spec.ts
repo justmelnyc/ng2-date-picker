d72a1105f8f9402bbebd1d851adb92cdcd280dca
Vlad Ioffe <vlio20@gmail.com>
2017-03-08 23:46:50 +0200

    adding back dist files

--- src/app/dp-calendar/dp-calendar.component.spec.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async } from '@angular/core/testing';
-import { DpCalendarComponent } from './dp-calendar.component';
-
-describe('Component: ObCalendar', () => {
-
-});
--- src/app/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './dp-date-picker.module';
